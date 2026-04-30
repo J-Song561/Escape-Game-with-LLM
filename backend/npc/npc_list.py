185b67c000424c29bc718365c06e53c9247b2636
June Song 송혜준 <annesong03@gmail.com>
2026-04-30 00:55:06 +0000

    npc_list

---
+++ backend/npc/npc_list.py
@@ -0,0 +1,20 @@
+NPC_LIST = {
+    "ella": {
+        "name": "엘라 부인",
+        "description": "저택의 안주인. 고상하지만 위험한 인물.",
+    },
+
+    "louis": {
+        "name": "집사 루이스",
+        "description": "20년 근속의 수석 집사. 말이 없고 모든 것을 알고 있다.",
+    },
+
+    "hailey": {
+        "name": "헤일리",
+        "description": "엘라 부인의 딸. 해맑고 수다스럽지만 아는 것이 없다.",
+    },
+
+    
+}
+
+VALID_NPCS = list(NPC_LIST.keys())  # ["ella", "louis", "hailey"]
